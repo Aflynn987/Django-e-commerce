8bcc8c86bf5e65d5129e8fcb65ef4eb562f7cfba
Aflynn987 <aflynn987@gmail.com>
2023-04-02 12:33:29 +0100

    Implement poor logging practices in Users that logs sensitive information in plain text

--- e_commerce/users/views.py
+++ e_commerce/users/views.py
@@ -11,6 +11,8 @@ from django.contrib import messages
 
 from e_commerce_app.models import Product
 from .forms import PurchaseForm
+import logging
+
 
 # Create your views here.
 
@@ -18,6 +20,8 @@ def logout_view(request):
     """Log the user out"""
     logout(request)
     return HttpResponseRedirect(reverse('e_commerce_app:index'))
+
+logger = logging.getLogger(__name__)
 
 def register(request):
     """Register a new user"""
@@ -30,22 +34,24 @@ def register(request):
 
         if form.is_valid():
             new_user = form.save()
+            logger.info('New user created: %s', new_user.username)
             # Log the user in and then redirect to home page
             authenticated_user = authenticate(username=new_user.username,
                     password=request.POST['password1'])
             login(request, authenticated_user)
+            logger.info('User %s logged in', new_user.username)
             return HttpResponseRedirect(reverse('e_commerce_app:index'))
+        else:
+            logger.error('Invalid registration form submission')
 
     context = {'form': form}
     return render(request, 'users/register.html', context)
 
-@login_required
 def personal_details(request, product_id):
     product = Product.objects.get(id=product_id)
     context = {'product': product, 'product_id': product_id}
     return render(request, 'users/personal_details.html', context)
 
-@login_required
 def user_profile(request):
     user = request.user
 
@@ -90,7 +96,6 @@ def user_profile(request):
     }
     return render(request, 'users/user_profile.html', context)
 
-@login_required
 def purchase_product(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     if request.method == 'POST':
@@ -108,9 +113,13 @@ def purchase_product(request, product_id):
             request.session['card_expiry'] = form.cleaned_data['card_expiry']
             request.session['card_cvv'] = form.cleaned_data['card_cvv']
             request.save()
+
+            # Poor logging practice: log sensitive user data as plain text
+            logger.info(
+                f"New purchase made: {request.session['full_name']}, {request.session['email']}, {request.session['address1']}, {request.session['address2']}, {request.session['city']}, {request.session['state']}, {request.session['zip_code']}, {request.session['card_name']}, {request.session['card_number']}, {request.session['card_expiry']}, {request.session['card_cvv']}")
+
             return redirect('e_commerce_app:purchase_product', product_id=product_id)
     else:
         form = PurchaseForm()
 
     return render(request, 'e_commerce_app/purchase_product.html', {'form': form, 'product': product})
-
